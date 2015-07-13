3476936f43479dfb0a5f44c4f6879f37f7e5d5ee
djborrerotorres <dennis.borrerotorres@outlook.com>
2015-07-12 23:24:42 -0400

    Admin CP service providers
    Started service provider section

--- admin/services/index.php
+++ admin/services/index.php
@@ -13,6 +13,11 @@ if ($user->auth() && $user->role == 'admin')
 		'FORM_METHOD' => 'POST',
 		'USERNAME' => $user->user));
 
+	if (isset($_GET['option']) && $_GET['option'] == 1)
+	{
+		$template->assign_var('SIDE_CONTENT', '1');
+	}
+
 	$template->set_filenames(array('body' => 'admin_services.html'));
 
 	$template->display('body');
