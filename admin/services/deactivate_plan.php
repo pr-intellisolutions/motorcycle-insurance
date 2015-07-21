1eddd0fa712d6dee5d58b8735094d136ff3e6356
djborrerotorres <dennis.borrerotorres@outlook.com>
2015-07-21 08:27:45 -0400

    Added FPDF library
    Added FPDF library to generate reports in PDF.

--- admin/services/deactivate_plan.php
+++ admin/services/deactivate_plan.php
@@ -5,6 +5,12 @@ require_once('./plan.php');
 
 $plan_name = isset($_POST['plan_name']) ? $_POST['plan_name'] : "";
 
+if ($plan_name === "")
+{
+	echo "Nada seleccionado.";
+	die();
+}
+
 $plan = new Plan;
 
 if ($plan->activate_plan($plan_name, false))
