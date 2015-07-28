f8c5d4cc5adc0c2780314d894ccfdc8f66105e32
djborrerotorres <dennis.borrerotorres@outlook.com>
2015-07-28 08:40:59 -0400

    road assistance interface update
    Added the road assistance interface to generate service orders

--- admin/assistance/index.php
+++ admin/assistance/index.php
@@ -8,13 +8,23 @@ $template->assign_var('SITE_URL', SITE_URL);
 
 if ($user->auth() && $user->role == 'admin')
 {
-	$template->assign_vars(array('SIDE_CONTENT' => 'home',
-		'FORM_ACTION' => $_SERVER['PHP_SELF'],
+	$template->assign_vars(array('FORM_ACTION' => $_SERVER['PHP_SELF'],
 		'FORM_METHOD' => 'POST',
 		'USERNAME' => $user->user));
 
+	if (isset($_GET['option']) && $_GET['option'] == 1)
+	{
+		$template->assign_var('SIDE_CONTENT', 1);
+	}
+	else if (isset($_GET['option']) && $_GET['option'] == 2)
+	{
+		$template->assign_var('SIDE_CONTENT', 2);
+	}
+	else
+	{
+		$template->assign_var('SIDE_CONTENT', 'home');
+	}		
 	$template->set_filenames(array('body' => 'admin_assistance.html'));
-
 	$template->display('body');
 }
 else
