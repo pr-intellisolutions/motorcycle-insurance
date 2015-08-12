7f2b3d06707a088022900112c1815f20f3c42459
djborrerotorres <djborrerotorres@users.noreply.github.com>
2015-08-12 18:43:21 -0400

    Update index.php

--- admin/finances/index.php
+++ admin/finances/index.php
@@ -4,17 +4,15 @@ require_once('../../common.php');
 
 $template->set_custom_template(DIR_BASE.'styles', 'default');
 
-$template->assign_var('SITE_URL', SITE_URL);
-
-if ($user->auth() && $user->role == 'admin')
+if ($user->auth() && $user->role === 'admin')
 {
-	$template->assign_vars(array('SIDE_CONTENT' => 'home',
+	$template->assign_vars(array('SITE_URL', SITE_URL,
+		'SIDE_CONTENT' => 'home',
 		'FORM_ACTION' => $_SERVER['PHP_SELF'],
 		'FORM_METHOD' => 'POST',
 		'USERNAME' => $user->user));
 
 	$template->set_filenames(array('body' => 'admin_finances.html'));
-
 	$template->display('body');
 }
 else
