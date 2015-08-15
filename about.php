0d041589fade3e9cea77c6ad98852822fe66bf72
djborrerotorres <dennis.borrerotorres@outlook.com>
2015-08-15 17:23:03 -0400

    user interface update

---
+++ about.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once('./common.php');
+
+$template->set_custom_template(DIR_BASE.'styles', 'default');
+
+$template->assign_var('SITE_URL', SITE_URL);
+
+if ($user->auth())
+{
+	$stmnt = sprintf("SELECT first, middle, last, maiden FROM profile INNER JOIN login ON profile.userid = login.id WHERE profile.userid=%d", $user->user_id);
+	
+	$result = $user->sql_conn->query($stmnt);
+	
+	if ($result->num_rows > 0)
+	{
+		$row = $result->fetch_assoc();
+
+		$template->assign_var('USERNAME', sprintf("%s %s %s %s", $row['first'], $row['middle'], $row['last'], $row['maiden']));
+
+		$result->close();
+	}
+	$template->assign_vars(array('USER_AUTH_VALID' => true,
+		'USER_ROLE' => $user->role));
+}
+else
+{
+	$template->assign_vars(array('USER_AUTH_VALID' => false,
+		'USER_ROLE' => $user->role));
+}
+
+$template->set_filenames(array('body' => 'about.html'));
+$template->display('body');
+
+?>
