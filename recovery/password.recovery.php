01ce7851c194dcd54156f602b379ded11a483ae1
djborrerotorres <dennis.borrerotorres@outlook.com>
2015-09-13 12:55:11 -0400

    process payment update

--- recovery/password.recovery.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('../common.php');
-
-if ($user->auth())
-{
-	header('Location: '.SITE_URL);
-	die;
-}
-?>
--- shop/index.php
+++ shop/index.php
@@ -21,6 +21,22 @@ if ($user->auth() && (isset($_SESSION['buy_plan']) && $_SESSION['buy_plan'] == t
 		$result->close();
 	}
 
+	$stmnt = sprintf("SELECT title, description, plan_price, extend_price FROM plans WHERE id=%d", $_SESSION['selected_plan']);
+	
+	$result = $user->sql_conn->query($stmnt);
+	
+	if ($result->num_rows > 0)
+	{
+		$row = $result->fetch_assoc();
+		
+		$template->assign_vars(array('PLAN_TITLE' => $row['title'],
+			'PLAN_DESC' => $row['description'],
+			'PLAN_PRICE' => $row['plan_price'],
+			'PLAN_EXTENDED' => $row['extend_price']));
+
+		$result->close();
+	}
+
 	$template->assign_vars(array('USER_AUTH_VALID' => true,
 		'USER_ROLE' => $user->role,
 		'NO_LOGIN_INFO' => false));
